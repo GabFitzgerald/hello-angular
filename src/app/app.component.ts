0237a29c134c167a046c3347f55ee39f9de772f6
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-08-24 11:58:00 +1000

    idk what I did but it's broken

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+// this is a decorator, it doesn't require a decorator at the end!
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<h1>Artist</h1>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-angular';
+  title = 'Hello Angular!';
 }
